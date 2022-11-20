3a470e6eb65419e35afedec47d96ce3f1650d1f8
F4bioo <costa.fbo@gmail.com>
2022-11-20 15:21:42 -0300

    Changed: [VCEP-TRIVIAL] Muda nome do módulo register

--- features/register/src/main/java/com/fappslab/viacep/register/domain/repository/RegisterRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.fappslab.viacep.register.domain.repository
-
-import com.fappslab.viacep.register.domain.model.Address
-import kotlinx.coroutines.flow.Flow
-
-interface RegisterRepository {
-
-    fun getAddress(zipcode: String): Flow<Address>
-}
